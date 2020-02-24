9c130a001f7efb4ffd3c5ad874265da1c2ecd634
Rubén <rubenalb90@gmail.com>
2020-02-24 21:06:29 +0100

    Updates

---
+++ precache-manifest.5de7a0e704291d1a2eaebb93188643ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a4fcd810ad57b349b66148fdae9c0f",
+    "url": "/Where-Watch/index.html"
+  },
+  {
+    "revision": "4bd4db02d6a1c0742f22",
+    "url": "/Where-Watch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "353c24241217394c7ac0",
+    "url": "/Where-Watch/static/js/2.65524267.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Where-Watch/static/js/2.65524267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd4db02d6a1c0742f22",
+    "url": "/Where-Watch/static/js/main.ce1fc85a.chunk.js"
+  },
+  {
+    "revision": "ad2fb364dcadc92c4bcd",
+    "url": "/Where-Watch/static/js/runtime-main.00808257.js"
+  },
+  {
+    "revision": "b5f314088a930692fd0d1c01d46a0731",
+    "url": "/Where-Watch/static/media/logo.b5f31408.jpg"
+  }
+]);
